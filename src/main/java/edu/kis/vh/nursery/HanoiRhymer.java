539e067fd23c493315c1cbf8a36435cd9af501f8
qushishu <krzysiek22.00@o2.pl>
2021-12-21 19:31:17 +0100

    format

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -11,7 +11,7 @@ int totalRejected = 0;
 	public void countIn(int in) {
 	if (!callCheck() && in > peekaboo())
 			totalRejected++;
-			else
-				super.countIn(in);
+	else
+		super.countIn(in);
 	}
 }
